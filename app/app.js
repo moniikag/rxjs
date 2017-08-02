8627d8049f45f8b3c08e3f50e6c3fcfa8cfdae63
Monika Glier <monikaglier@gmail.com>
2017-08-02 10:45:09 +0200

    Render response data on the DOM with RxJS

--- app/app.js
+++ app/app.js
@@ -9,6 +9,33 @@ let responseStream = requestStream
     )
   )
 
-responseStream.subscribe(response => {
-  console.log(response)
+function createSuggestionStream(responseStream) {
+  return responseStream.map(listUser =>
+    listUser[Math.floor(Math.random()*listUser.length)]
+  )
+}
+
+let suggestion1Stream = createSuggestionStream(responseStream)
+let suggestion2Stream = createSuggestionStream(responseStream)
+let suggestion3Stream = createSuggestionStream(responseStream)
+
+function renderSuggestion(userData, selector) {
+  const element = document.querySelector(selector)
+  let usernameEl = element.querySelector('.username')
+  usernameEl.href = userData.html_url
+  usernameEl.textContent = userData.login
+  let imgEl = element.querySelector('img')
+  imgEl.src = userData.avatar_url
+}
+
+suggestion1Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion1')
+})
+
+suggestion2Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2')
+})
+
+suggestion3Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion3')
 })
