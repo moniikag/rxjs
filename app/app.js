c5dfc66050a42cf85ec4602b9fdda2d38292862b
Monika Glier <monikaglier@gmail.com>
2017-08-08 10:50:31 +0200

    Use latest data from a stream (cache)

--- app/app.js
+++ app/app.js
@@ -1,7 +1,14 @@
 import Rx from 'rxjs'
 
 let refreshButton = document.querySelector('.refresh')
+const closeButton1 = document.querySelector('.close1')
+const closeButton2 = document.querySelector('.close2')
+const closeButton3 = document.querySelector('.close3')
+
 let refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click')
+let close1Clicks = Rx.Observable.fromEvent(closeButton1, 'click')
+let close2Clicks = Rx.Observable.fromEvent(closeButton2, 'click')
+let close3Clicks = Rx.Observable.fromEvent(closeButton3, 'click')
 
 let startupRequestStream = Rx.Observable.of('https://api.github.com/users')
 
@@ -11,7 +18,9 @@ let requestOnRefreshStream = refreshClickStream
     return 'https://api.github.com/users?since=' + randomOffset
   })
 
-let responseStream = requestOnRefreshStream.merge(startupRequestStream)
+let requestStream = requestOnRefreshStream.merge(startupRequestStream)
+
+let responseStream = requestStream
   .flatMap(requestUrl => {
     console.log('request')
     return Rx.Observable.fromPromise(
@@ -20,17 +29,26 @@ let responseStream = requestOnRefreshStream.merge(startupRequestStream)
   })
   .shareReplay()
 
-function createSuggestionStream(responseStream) {
-  return responseStream.map(listUser =>
-    listUser[Math.floor(Math.random()*listUser.length)]
-  )
-  .startWith(null)
-  .merge(refreshClickStream.map(ev => null))
+// refreshClickStream: -------f--------------->
+// requestStream:      r------r--------------->
+// responseStream:     ---R------R------------>
+// closeClickStream:   ---------------x------->
+// suggestion1Stream:  N--u---N--u----u------->
+
+const getRandomUser = (users) => users[Math.floor(Math.random()*users.length)]
+
+function createSuggestionStream(responseStream, closeClickStream) {
+  return responseStream.map(listUser => getRandomUser(listUser))
+    .startWith(null)
+    .merge(refreshClickStream.map(ev => null))
+    .merge(closeClickStream
+            .withLatestFrom(responseStream, (ev, listUsers) => getRandomUser(listUsers))
+          )
 }
 
-let suggestion1Stream = createSuggestionStream(responseStream)
-let suggestion2Stream = createSuggestionStream(responseStream)
-let suggestion3Stream = createSuggestionStream(responseStream)
+let suggestion1Stream = createSuggestionStream(responseStream, close1Clicks)
+let suggestion2Stream = createSuggestionStream(responseStream, close2Clicks)
+let suggestion3Stream = createSuggestionStream(responseStream, close3Clicks)
 
 function renderSuggestion(userData, selector) {
   const element = document.querySelector(selector)
