8c57bb5a4b7e323cd6b084cadbe472396d070284
Monika Glier <monikaglier@gmail.com>
2017-08-11 12:12:52 +0200

    Use takeUntil interval instead of unsubscribe

--- app/app.js
+++ app/app.js
@@ -2,8 +2,17 @@ import Rx from 'rxjs'
 
 const click = Rx.Observable.fromEvent(document, 'click')
 
-const subscription = click.subscribe((ev) => {
+const four = Rx.Observable.interval(4000).take(1)
+
+const clickUntilFour = click.takeUntil(four)
+
+/*
+  click             ----c--c--c----c-c-c--c---c-c-c-c---
+  four              --------------------0|
+  clickUntilFour    ----c--c--c----c-c-c|
+*/
+
+const subscription = clickUntilFour.subscribe((ev) => {
   console.log(ev.clientX)
 })
 
-setTimeout(() => subscription.unsubscribe(), 4000)
