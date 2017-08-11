1f9b73d58359c7efc54a0710de8f9afe52df4f5e
Monika Glier <monikaglier@gmail.com>
2017-08-11 12:27:15 +0200

    Take x elements

--- app/app.js
+++ app/app.js
@@ -2,17 +2,14 @@ import Rx from 'rxjs'
 
 const click = Rx.Observable.fromEvent(document, 'click')
 
-const four = Rx.Observable.interval(4000).take(1)
-
-const clickUntilFour = click.takeUntil(four)
+const sixClicks = click.take(6)
 
 /*
   click             ----c--c--c----c-c-c--c---c-c-c-c---
-  four              --------------------0|
-  clickUntilFour    ----c--c--c----c-c-c|
+  sixClicks         ----c--c--c----c-c-c|
 */
 
-const subscription = clickUntilFour.subscribe((ev) => {
+sixClicks.subscribe((ev) => {
   console.log(ev.clientX)
 })
 
