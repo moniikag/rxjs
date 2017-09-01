912bd0b321e999e322235f49248481c644794633
Monika Glier <monikaglier@gmail.com>
2017-09-01 09:59:47 +0200

    Extending observables

--- app/app.js
+++ app/app.js
@@ -1,28 +1,78 @@
 import Rx from 'rxjs'
 
-// const res$ = Rx.Observable.interval(2000)
-//   .switchMap(ev => Rx.Observable.ajax({
-//     url: 'https://jsonplaceholder.typicode.com/users/1',
-//     method: 'GET',
-//   }))
+// wrong. we shouldn't be building own observables in such cases
 
-// res$.subscribe(data => console.log(data))
+// class MyAgeObservable extends Rx.Observable {
+//   constructor(initialAge) {
+//     super((observer) => {
+//       observer.next(initialAge)
+//       this.observer = observer  // but: observables don't carry state between different observers
+//     })
+//     this.age = initialAge
+//   }
 
-const resume$ = new Rx.Subject()
+//   setAge(age) {
+//     this.age = age
+//     this.observer.next(age)
+//   }
+// }
 
-const res$ = resume$
-  .switchMap(resume => resume ?
-                          Rx.Observable.interval(2000) : // after pause, the new one will always start from 0
-                          Rx.Observable.empty() // empty stream, nothing to map through
-            )
-  .do(x => console.log('request it! ' + x))
-  .switchMap(ev => Rx.Observable.ajax({
-    url: 'https://jsonplaceholder.typicode.com/users/1',
-    method: 'GET',
-  }))
+// const a$ = new MyAgeObservable(20)
 
-res$.subscribe(data => console.log(data))
+// a$.subscribe(x => console.log('My age is ' + x))
+// a$.setAge(21)
 
-resume$.next(false)
-setTimeout(() => resume$.next(true), 500)
-setTimeout(() => resume$.next(false), 5000)
+
+// correct. we can create our own observable if we want to inject a special
+// behaviour throughout the whole chain
+
+class LogSubscriber extends Rx.Subscriber {
+  next(value) {
+    console.log('next ' + value)
+    this._next(value)
+  }
+
+  error(e) {
+    console.log('error ' + e)
+    this._error(e)
+  }
+
+  complete() {
+    console.log('complete')
+    this._complete()
+  }
+}
+
+class LogOperator {
+  constructor(childOperator) {
+    this.childOperator = childOperator
+  }
+
+  call(subscriber, source) {
+    return this.childOperator.call(
+      new LogSubscriber(subscriber), source
+    )
+  }
+}
+
+class LogObservable extends Rx.Observable {
+  lift(operator) {
+    const observable = new LogObservable()
+    observable.source = this
+    observable.operator = new LogOperator(operator)
+    return observable
+  }
+}
+
+const observable = new LogObservable((observer) => {
+  setTimeout(() => {observer.next(1)}, 100)
+  setTimeout(() => {observer.next(2)}, 200)
+  setTimeout(() => {observer.next(3)}, 300)
+  setTimeout(() => {observer.complete()}, 400)
+})
+
+observable
+  .map(x => 10 * x) // result: LogObservable
+  .filter(x => x > 15) // result: LogObservable
+  .count() // result: LogObservable
+  .subscribe(x => { alert(x) })
