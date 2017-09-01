547a4132df2225b02eeda040d159b72b587c3a20
Monika Glier <monikaglier@gmail.com>
2017-09-01 09:19:46 +0200

    Implement pause & resume with RxJS

--- app/app.js
+++ app/app.js
@@ -1,33 +1,28 @@
 import Rx from 'rxjs'
 
-const dotElement = document.getElementById('dot')
-
-const updateDot = (x, y) => {
-  dotElement.style.left = `${x}px`
-  dotElement.style.top = `${y}px`
-}
-
-// Rx.Observable.fromEvent(document, 'click')
-//   // .do returns the same as it received,
-//   // but it allows to add side effects.
-//   // so it's like a map that does sth before returning unmodified ev
-//   .do(ev => updateDot(ev.clientX, ev.clientY))
+// const res$ = Rx.Observable.interval(2000)
 //   .switchMap(ev => Rx.Observable.ajax({
 //     url: 'https://jsonplaceholder.typicode.com/users/1',
 //     method: 'GET',
 //   }))
-//   .subscribe(data => console.log(data.response))
 
-const click$ = Rx.Observable.fromEvent(document, 'click')
+// res$.subscribe(data => console.log(data))
 
-click$.subscribe(ev => updateDot(ev.clientX, ev.clientY))
+const resume$ = new Rx.Subject()
 
-const res$ = click$
+const res$ = resume$
+  .switchMap(resume => resume ?
+                          Rx.Observable.interval(2000) : // after pause, the new one will always start from 0
+                          Rx.Observable.empty() // empty stream, nothing to map through
+            )
+  .do(x => console.log('request it! ' + x))
   .switchMap(ev => Rx.Observable.ajax({
     url: 'https://jsonplaceholder.typicode.com/users/1',
     method: 'GET',
   }))
 
-res$.subscribe(data => console.log(data.response))
+res$.subscribe(data => console.log(data))
 
-// use 'do' for debugging
+resume$.next(false)
+setTimeout(() => resume$.next(true), 500)
+setTimeout(() => resume$.next(false), 5000)
