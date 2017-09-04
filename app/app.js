d59e4845203ad6a467235700688f5082d8585869
Monika Glier <monikaglier@gmail.com>
2017-09-04 10:24:15 +0200

    Use shared Observables

--- app/app.js
+++ app/app.js
@@ -1,78 +1,20 @@
 import Rx from 'rxjs'
 
-// wrong. we shouldn't be building own observables in such cases
+// Each operates on all elements of Observable
 
-// class MyAgeObservable extends Rx.Observable {
-//   constructor(initialAge) {
-//     super((observer) => {
-//       observer.next(initialAge)
-//       this.observer = observer  // but: observables don't carry state between different observers
-//     })
-//     this.age = initialAge
-//   }
+// const clock$ = Rx.Observable.interval(1000).take(6)
 
-//   setAge(age) {
-//     this.age = age
-//     this.observer.next(age)
-//   }
-// }
+// clock$.subscribe(x => console.log('A: ' + x))
 
-// const a$ = new MyAgeObservable(20)
+// setTimeout(() => { clock$.subscribe(x => console.log('  B: ' + x))
+// }, 2500)
 
-// a$.subscribe(x => console.log('My age is ' + x))
-// a$.setAge(21)
+// Observable is shared by subscribers.
+// It does not invoke new execution of Observable, uses the shared one.
 
+const clock$ = Rx.Observable.interval(1000).take(6).share()
 
-// correct. we can create our own observable if we want to inject a special
-// behaviour throughout the whole chain
+clock$.subscribe(x => console.log('A: ' + x))
 
-class LogSubscriber extends Rx.Subscriber {
-  next(value) {
-    console.log('next ' + value)
-    this._next(value)
-  }
-
-  error(e) {
-    console.log('error ' + e)
-    this._error(e)
-  }
-
-  complete() {
-    console.log('complete')
-    this._complete()
-  }
-}
-
-class LogOperator {
-  constructor(childOperator) {
-    this.childOperator = childOperator
-  }
-
-  call(subscriber, source) {
-    return this.childOperator.call(
-      new LogSubscriber(subscriber), source
-    )
-  }
-}
-
-class LogObservable extends Rx.Observable {
-  lift(operator) {
-    const observable = new LogObservable()
-    observable.source = this
-    observable.operator = new LogOperator(operator)
-    return observable
-  }
-}
-
-const observable = new LogObservable((observer) => {
-  setTimeout(() => {observer.next(1)}, 100)
-  setTimeout(() => {observer.next(2)}, 200)
-  setTimeout(() => {observer.next(3)}, 300)
-  setTimeout(() => {observer.complete()}, 400)
-})
-
-observable
-  .map(x => 10 * x) // result: LogObservable
-  .filter(x => x > 15) // result: LogObservable
-  .count() // result: LogObservable
-  .subscribe(x => { alert(x) })
+setTimeout(() => { clock$.subscribe(x => console.log('  B: ' + x))
+}, 2500)
