07318e8310662baac140bb7376c9bc0176a326f8
Monika Glier <monikaglier@gmail.com>
2017-09-12 12:23:46 +0200

    AsyncSubject - replays one, only if completed

--- app/app.js
+++ app/app.js
@@ -1,30 +1,13 @@
 import Rx from 'rxjs'
 
-// const bufferSize = 2
-const bufferSize = Number.POSITIVE_INFINITY // => all that happened in the past
+var subject = new Rx.AsyncSubject();
 
-const windowSize = 250
-/*
-windowSize - determins how long in time will the ReplaySubject keep events
-stored in its internal buffer
-*/
+// Subject
+// ReplaySubject: replays many, before or after completion
+// BehaviorSubject: replays one, only before completion
+// AsyncSubject: replays one, only if completed
 
-const subject = new Rx.ReplaySubject(bufferSize, windowSize);
-// Replays everything once a new subscriber arrives.
-// Even if first observer has already completed.
-
-/*
-BehaviorSubject !== ReplaySubject(1)
-- ReplaySubject does not have an initial value nor current value,
-it just replays events from the past.
-ReplaySubject can replay even when first observer has completed.
-For BehaviorSubject, after first observer has completed - there is no value.
-
-=> to represent value over time: BehaviorSubject
-=> to replay events: ReplaySubject
-*/
-
-const observerA = {
+var observerA = {
   next: function (x) { console.log('A next ' + x); },
   error: function (err) { console.log('A error ' + err); },
   complete: function () { console.log('A done'); },
@@ -33,7 +16,7 @@ const observerA = {
 subject.subscribe(observerA);
 console.log('observerA subscribed');
 
-const observerB = {
+var observerB = {
   next: function (x) { console.log('B next ' + x); },
   error: function (err) { console.log('B error ' + err); },
   complete: function () { console.log('B done'); },
@@ -46,8 +29,10 @@ setTimeout(() => subject.complete(), 350);
 
 /*
 ----1---2---3--|
-  ..1...2...3...
-                 1,2,3|
+  .............3|
+                   3|
+
+=> all subscribers only get the last value
 */
 
 setTimeout(function () {
