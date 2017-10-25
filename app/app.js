c98fe45ab4da70200ff321435205b2b0698651b6
Monika Glier <monikaglier@gmail.com>
2017-10-25 14:11:37 +0200

    Stop a stream with .takeUntil

--- app/app.js
+++ app/app.js
@@ -2,20 +2,15 @@ import Rx from 'rxjs'
 import { Observable } from 'rxjs/Rx'
 
 const startButton = document.querySelector('#start')
-
-// switchMap here switches us over to another Observable
-// you can still access click event, e.g.:
-// .switchMap((event) => Obeservable.interval(event.x))
-
-// Observable.fromEvent(startButton, 'click')
-//   .switchMap(() => Observable.interval(1000))
-//   .subscribe((x) => console.log(x))
+const stopButton = document.querySelector('#stop')
 
 const start$ = Observable.fromEvent(startButton, 'click')
 const interval$ = Observable.interval(1000)
-// You can use switchMapTo instead of switchMap with arrow function
-// const startInterval$ = start$.switchMap(() => interval$)
-const startInterval$ = start$.switchMapTo(interval$)
+const stop$ = Observable.fromEvent(stopButton, 'click')
+const intervalThatStops$ = interval$.takeUntil(stop$)
 
-startInterval$
-  .subscribe((x) => console.log(x))
+// intervalThatStops$.subscribe(x => console.log(x))
+
+start$
+  .switchMapTo(intervalThatStops$)
+  .subscribe(x => console.log(x))
