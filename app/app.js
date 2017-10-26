ebe899bf4af2a9adb3ebcb2a16f9357c1b96d980
Monika Glier <monikaglier@gmail.com>
2017-10-26 10:52:46 +0200

    Use .startWith for Observable to start with an initial value

--- app/app.js
+++ app/app.js
@@ -12,9 +12,16 @@ const intervalThatStops$ = interval$.takeUntil(stop$)
 // .scan works like JavaScript reduce
 // takes function & initializer
 
+// .startWith will pass the initial data as scan's acc
+// will fire one time with 0 instead of waiting for start
+// might be useful e.g. for timers which we want to initialize with 0
+
+const data = { count: 0 }
+
 start$
   .switchMapTo(intervalThatStops$)
+  .startWith(data)
   .scan((acc) => {
     return { count: acc.count + 1 }
-  }, { count: 0 })
+  })
   .subscribe(x => console.log(x))
