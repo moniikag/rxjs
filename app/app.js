d91f15a2394f067647fc46d69b347a722823e492
Monika Glier <monikaglier@gmail.com>
2017-10-26 11:38:46 +0200

    Handle multiple streams with .merge

--- app/app.js
+++ app/app.js
@@ -3,24 +3,26 @@ import { Observable } from 'rxjs/Rx'
 
 const startButton = document.querySelector('#start')
 const stopButton = document.querySelector('#stop')
+const resetButton = document.querySelector('#reset')
 
 const start$ = Observable.fromEvent(startButton, 'click')
 const interval$ = Observable.interval(1000)
 const stop$ = Observable.fromEvent(stopButton, 'click')
-const intervalThatStops$ = interval$.takeUntil(stop$)
+const reset$ = Observable.fromEvent(resetButton, 'click')
 
+const intervalThatStops$ = interval$.takeUntil(stop$)
 
 const data = { count: 0 }
 const inc = (acc) => ({ count: acc.count + 1 })
 const reset = (acc) => data
 
-// .scan receives:
-// 1) accumulator,
-// 2) current value - which we can set to a function i.e. by using .mapTo
+const incOrRest$ = Observable.merge(
+  intervalThatStops$.mapTo(inc),
+  reset$.mapTo(reset)
+)
 
 start$
-  .switchMapTo(intervalThatStops$)
-  .mapTo(inc)
+  .switchMapTo(incOrRest$)
   .startWith(data)
   .scan((acc, curr) => curr(acc))
   .subscribe(x => console.log(x))
